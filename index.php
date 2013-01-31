8d9f95df8cda5e9d3223644f087cf90b823fe5d6
Bill Tinon <btinon@gmail.com>
2013-01-30 20:17:31 -0700

    add shading

--- index.php
+++ index.php
@@ -6,30 +6,50 @@
   <title>Budge</title>
   <link rel="stylesheet" type="text/css" href="main.css">
 </head>
-<body>
+<body bgcolor="#cccccc">
   <div id="container">
-    <h2>    Credit                       Amount</h2>
     <form action="write.php" method="post">
     <div id="credit">
+      <table>
+        <th>Credit</th><th>Amount</th>
       <?php
       for ($c=0; $c<6; $c++)
       {
-        echo '<input type="text" size="60" name="credit'.$c.'">\t\t';
-        echo '<input type="text size="10" name="credamt'.$c.'"><br /><br />';
+        echo '<tr>';
+        echo '<td><input class="shade" type="text" size="60" name="credit'.$c.'"></td>';
+        echo '<td><input class="shade" type="text size="10" name="credamt'.$c.'"></td>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td colspan="2">&nbsp;</td>';
+        echo '</tr>';
+
       }
       ?>
+    </table>
     </div>
+    <p />
+    <p />
     <div id="expense">
+      <table>
+        <th>Expense</th><th>Amount</th>
+      <?php
+      for ($x=0; $x<11; $x++)
+      {
+        echo '<tr>';
+        echo '<td><input class="shade" type="text" size="60" name="expense'.$x.'"></td>';
+        echo '<td><input class="shade" type="text size="10" name="expamt'.$x.'"></td>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td colspan="2">&nbsp;</td>';
+        echo '</tr>';
+      }
+      ?>
+    </table>
       
-        <?php
-        for ($x=0; $x<12; $x++)
-        {
-          echo '<input type="text" size="60" name="expense' . $x . '">' . "\t\t";
-          echo '<input type="text size="10" name="expamt'.$x.'"><br /><br />';
-        }
-        ?>
-
-      
+    </div>
+    <p />
+    <div id="submit">
+      <input type="submit" value="submit">
     </div>
     </form>
   </div>

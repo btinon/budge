cee97db9c472df4f782132869ab5a8fb89032dcf
Bill Tinon <btinon@gmail.com>
2013-01-30 19:35:40 -0700

    add credit form

--- index.php
+++ index.php
@@ -8,21 +8,30 @@
 </head>
 <body>
   <div id="container">
-    <div id="expense">
-      <form action="write.php" method="post">
-        <?php
-        for ($x=0; $x<15; $x++)
-        {
-          echo '<input type="text" size="60" name="expense'.$x.'">\t\t';
-          echo '<input type="text size="10" name="amount'.$x.'"><br />';
-        }
-
-        
-        ?>
-      </form>
-    </div>
+    <h2>    Credit                       Amount</h2>
+    <form action="write.php" method="post">
     <div id="credit">
+      <?php
+      for ($c=0; $c<6; $c++)
+      {
+        echo '<input type="text" size="60" name="credit'.$c.'">\t\t';
+        echo '<input type="text size="10" name="credamt'.$c.'"><br /><br />';
+      }
+      ?>
     </div>
+    <div id="expense">
+      
+        <?php
+        for ($x=0; $x<12; $x++)
+        {
+          echo '<input type="text" size="60" name="expense' . $x . '">' . "\t\t";
+          echo '<input type="text size="10" name="expamt'.$x.'"><br /><br />';
+        }
+        ?>
+
+      
+    </div>
+    </form>
   </div>
 </body>
 </html>
